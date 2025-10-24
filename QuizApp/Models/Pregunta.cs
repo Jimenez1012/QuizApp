2cec102bc8d6d01e8be599d836ba88e49bc04597
Luis Enrique Jimenez Pinzon <23470152@campeche.tecnm.mx>
2025-10-24 15:40:57 -0600

    Se agrego la interfaz de usuario

---
+++ QuizApp/Models/Pregunta.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizApp.Models
+{
+    public class Pregunta
+    {
+        public string Texto { get; set; } = string.Empty;
+        public bool RespuestaEsCierto { get; set; }
+    }
+}
---
+++ QuizApp/Views/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace QuizApp.Views;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
